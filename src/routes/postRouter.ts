c872c50db9830599439c4d45f1968ab097597854
EliasOlie <eliasvieira31@gmail.com>
2022-05-24 11:10:33 -0300

    Refactor post router

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,13 +1,35 @@
 import { Router } from "express";
-import { PostsController } from "../Controllers/Posts";
-import { PrismaAdapter } from "../infra/prismaAdapter";
-
-const postsController = new PostsController(PrismaAdapter)
+import { createPostController } from "../Controllers/Posts/createPost";
+import { readPostsController } from "../Controllers/Posts/readPosts";
+import { readPostController } from "../Controllers/Posts/readPost";
+import { deletePostController } from "../Controllers/Posts/deletePost";
+import { updatePostContentController } from "../Controllers/Posts/updatePostContent";
+import { updatePostDescriptionController } from "../Controllers/Posts/updatePostDescription";
 
 const postRouter = Router()
 
 postRouter.post("/create", async (req, res) => {
-    await postsController.createPost(req, res)
+    await createPostController.run(req, res)
+})
+
+postRouter.get("/author/:author", async (req, res) => {
+    await readPostsController.run(req, res)
+})
+
+postRouter.get("/post/:id", async (req, res) => {
+    await readPostController.run(req, res)
+})
+
+postRouter.delete("/delete/:id", async (req, res) => {
+    await deletePostController.run(req, res)
+})
+
+postRouter.patch("/content/change", async (req, res) => {
+    await updatePostContentController.run(req, res)
+})
+
+postRouter.patch("/description/change", async (req, res) => {
+    await updatePostDescriptionController.run(req, res)
 })
 
 export { postRouter }
